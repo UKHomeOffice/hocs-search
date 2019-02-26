bd78cd0180b14021c8e4e2fdb13430cea0ba01fc
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-02-26 14:03:37 +0000

    blank project

---
+++ src/main/java/uk/gov/digital/ho/hocs/search/HocsSearchApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.digital.ho.hocs.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocsSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HocsSearchApplication.class, args);
+    }
+
+}
