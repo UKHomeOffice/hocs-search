6c809713b640211309f623117b4bccf3c14cdd09
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-03-04 15:13:42 +0000

    skeleton service + some tests

---
+++ src/main/java/uk/gov/digital/ho/hocs/search/api/dto/CreateCorrespondentRequest.java
@@ -0,0 +1,38 @@
+package uk.gov.digital.ho.hocs.search.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+public class CreateCorrespondentRequest {
+
+    @JsonProperty("uuid")
+    private UUID uuid;
+
+    @JsonProperty("created")
+    private LocalDateTime created;
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("fullname")
+    private String fullname;
+
+    @JsonProperty("address")
+    private AddressDto address;
+
+    @JsonProperty("telephone")
+    private String telephone;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("reference")
+    private String reference;
+
+}
---
+++ src/main/java/uk/gov/digital/ho/hocs/search/domain/repository/model/CaseData.java
@@ -0,0 +1,100 @@
+package uk.gov.digital.ho.hocs.search.domain.repository.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import uk.gov.digital.ho.hocs.search.api.dto.CreateCaseRequest;
+import uk.gov.digital.ho.hocs.search.api.dto.CreateCorrespondentRequest;
+import uk.gov.digital.ho.hocs.search.api.dto.UpdateCaseRequest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Document(indexName = "case", type = "caseData")
+public class CaseData {
+
+    @Id
+    private UUID caseUUID;
+
+    private LocalDateTime created;
+
+    private String type;
+
+    private String reference;
+
+    private UUID primaryTopic;
+
+    private UUID primaryCorrespondent;
+
+    private LocalDate caseDeadline;
+
+    private LocalDate dateReceived;
+
+    private Boolean deleted = false;
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private Set<Correspondent> currentCorrespondents = new HashSet<>();
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private Set<Correspondent> allCorrespondents = new HashSet<>();
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private Set<Topic> currentTopics = new HashSet<>();
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private Set<Topic> allTopics = new HashSet<>();
+
+    public CaseData(UUID uuid) {
+        this.caseUUID = uuid;
+    }
+
+    public void create(CreateCaseRequest createCaseRequest) {
+        this.created = createCaseRequest.getCreated();
+        this.type = createCaseRequest.getType();
+        this.reference = createCaseRequest.getReference();
+        this.caseDeadline = createCaseRequest.getCaseDeadline();
+        this.dateReceived = createCaseRequest.getDateReceived();
+    }
+
+    public void update(UpdateCaseRequest updateCaseRequest) {
+        this.created = updateCaseRequest.getCreated();
+        this.type = updateCaseRequest.getType();
+        this.reference = updateCaseRequest.getReference();
+        this.primaryTopic = updateCaseRequest.getPrimaryTopic();
+        this.primaryCorrespondent = updateCaseRequest.getPrimaryCorrespondent();
+        this.caseDeadline = updateCaseRequest.getCaseDeadline();
+        this.dateReceived = updateCaseRequest.getDateReceived();
+    }
+
+    public void delete() {
+        this.deleted = true;
+    }
+
+    public void addCorrespondent(CreateCorrespondentRequest createCorrespondentRequest) {
+        Correspondent correspondent = Correspondent.from(createCorrespondentRequest);
+        this.currentCorrespondents.add(correspondent);
+        this.allCorrespondents.add(correspondent);
+    }
+
+    public void removeCorrespondent(UUID correspondentUUID) {
+        this.currentCorrespondents.removeIf(c -> c.getUuid().equals(correspondentUUID));
+    }
+
+    public void addTopic(Topic topic) {
+        this.currentTopics.add(topic);
+        this.allTopics.add(topic);
+    }
+
+    public void removeTopic(UUID topicUUID) {
+        this.currentTopics.removeIf(c -> c.getUuid().equals(topicUUID));
+    }
+
+}
