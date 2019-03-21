21e06045a6fe88e71381022deb8b11cee1428fb6
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-03-21 15:44:54 +0000

    replace with rest backend

--- src/main/java/uk/gov/digital/ho/hocs/search/domain/model/Topic.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/domain/model/Topic.java
@@ -4,10 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 import uk.gov.digital.ho.hocs.search.client.infoclient.InfoTopic;
 
 import java.util.UUID;
@@ -15,13 +11,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Document(indexName = "topic", type = "topic")
 public class Topic {
 
-    @Id
     private UUID uuid;
 
-    @Field(type = FieldType.Text)
     private String text;
 
     public static Topic from(InfoTopic infoTopic) {
