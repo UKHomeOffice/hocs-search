f224a90b4e598b77d88338cc86e987144d2cdc8d
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-03-21 21:25:27 +0000

    force aws client to https

--- src/main/java/uk/gov/digital/ho/hocs/search/application/aws/ElasticSearchConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/application/aws/ElasticSearchConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local"})
+@Profile({"localelastic"})
 public class ElasticSearchConfiguration {
 
   @Value("${elasticsearch.host}")
