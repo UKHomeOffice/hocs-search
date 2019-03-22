9dfcc49b31f7b14f468857fe7f9be8c2b453fb36
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-03-22 11:04:20 +0000

    remove audit code

--- src/main/java/uk/gov/digital/ho/hocs/search/api/CaseDataService.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/api/CaseDataService.java
@@ -36,7 +36,7 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.create(createCaseRequest);
         elasticSearchClient.save(caseData);
-        log.debug("Created case {}", caseUUID);
+        log.info("Created case {}", caseUUID);
     }
 
     public void updateCase(UUID caseUUID, UpdateCaseRequest updateCaseRequest) {
@@ -44,7 +44,7 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.update(updateCaseRequest);
         elasticSearchClient.update(caseData);
-        log.debug("Updated case {}", caseUUID);
+        log.info("Updated case {}", caseUUID);
     }
 
     public void deleteCase(UUID caseUUID) {
@@ -52,7 +52,7 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.delete();
         elasticSearchClient.update(caseData);
-        log.debug("Deleted case {}", caseUUID);
+        log.info("Deleted case {}", caseUUID);
     }
 
     public void createCorrespondent(UUID caseUUID, CreateCorrespondentRequest createCorrespondentRequest) {
@@ -60,7 +60,7 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.addCorrespondent(createCorrespondentRequest);
         elasticSearchClient.update(caseData);
-        log.debug("Added correspondent {} to case {}", createCorrespondentRequest.getUuid(), caseUUID);
+        log.info("Added correspondent {} to case {}", createCorrespondentRequest.getUuid(), caseUUID);
     }
 
     public void deleteCorrespondent(UUID caseUUID, UUID correspondentUUID) {
@@ -68,7 +68,7 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.removeCorrespondent(correspondentUUID);
         elasticSearchClient.update(caseData);
-        log.debug("Deleted correspondent {} from case {}", correspondentUUID, caseUUID);
+        log.info("Deleted correspondent {} from case {}", correspondentUUID, caseUUID);
     }
 
     public void createTopic(UUID caseUUID, UUID topicUUID) {
@@ -77,7 +77,7 @@ public class CaseDataService {
         InfoTopic infoTopic = infoClient.getTopic(topicUUID);
         caseData.addTopic(Topic.from(infoTopic));
         elasticSearchClient.update(caseData);
-        log.debug("Added topic {} to case {}", topicUUID, caseUUID);
+        log.info("Added topic {} to case {}", topicUUID, caseUUID);
     }
 
     public void deleteTopic(UUID caseUUID, UUID topicUUID) {
@@ -85,11 +85,11 @@ public class CaseDataService {
         CaseData caseData = getCaseData(caseUUID);
         caseData.removeTopic(topicUUID);
         elasticSearchClient.update(caseData);
-        log.debug("Deleted topic {} from case {}", topicUUID, caseUUID);
+        log.info("Deleted topic {} from case {}", topicUUID, caseUUID);
     }
 
     Set<UUID> search(SearchRequest request){
-        log.debug("Searching for case");
+        log.info("Searching for case {}", request.toString());
         HocsQueryBuilder hocsQueryBuilder = new HocsQueryBuilder(QueryBuilders.boolQuery());
         hocsQueryBuilder.caseTypes(request.getCaseTypes());
         hocsQueryBuilder.dateRange(request.getDateReceived());
@@ -100,7 +100,7 @@ public class CaseDataService {
 
         Set<UUID> caseUUIDs =  elasticSearchClient.search(hocsQueryBuilder.build());
 
-        log.debug("Results {}", caseUUIDs.size());
+        log.info("Results {}", caseUUIDs.size());
         return caseUUIDs;
     }
 
