e513ba1d506fdbbf7dc8760673c066f3530d2d0e
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-08 15:18:13 +0100

    fix elasticsearch parity bug

--- src/main/java/uk/gov/digital/ho/hocs/search/HocsSearchApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/HocsSearchApplication.java
@@ -3,15 +3,11 @@ package uk.gov.digital.ho.hocs.search;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
-@EnableCaching
 @EnableRetry
-@EnableScheduling
 public class HocsSearchApplication {
 
     public static void main(String[] args) {
