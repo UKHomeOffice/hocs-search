30cba3a38241f211f4893563eb5cacb854e2224d
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2020-05-27 16:44:24 +0100

    HOCS-1751 Implementing the multiple token process on CI (separate service structure).

--- deploy.sh
+++ deploy.sh
@@ -7,26 +7,40 @@ if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
-if [[ ${ENVIRONMENT} == "prod" ]] ; then
-    echo "deploy ${VERSION} to PROD namespace, using HOCS_SEARCH_PROD drone secret"
-    export KUBE_TOKEN=${HOCS_SEARCH_PROD}
+if [[ ${KUBE_NAMESPACE} == "cs-prod" ]] ; then
+    echo "deploy ${VERSION} to PROD namespace, using HOCS_SEARCH_CS_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_CS_PROD}
     export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
+    echo "deploy ${VERSION} to PROD namespace, using HOCS_SEARCH_WCS_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_WCS_PROD}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
+    echo "deploy ${VERSION} to QA namespace, using HOCS_SEARCH_CS_QA drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_CS_QA}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "wcs-qa" ]] ; then
+    echo "deploy ${VERSION} to QA namespace, using HOCS_SEARCH_WCS_QA drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_WCS_QA}
+    export REPLICAS="2"
+elif [[ ${KUBE_NAMESPACE} == "cs-demo" ]] ; then
+    echo "deploy ${VERSION} to DEMO namespace, using HOCS_SEARCH_CS_DEMO drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_CS_DEMO}
+    export REPLICAS="1"
+elif [[ ${KUBE_NAMESPACE} == "wcs-demo" ]] ; then
+    echo "deploy ${VERSION} to DEMO namespace, using HOCS_SEARCH_WCS_DEMO drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_WCS_DEMO}
+    export REPLICAS="1"
+elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
+    echo "deploy ${VERSION} to DEV namespace, using HOCS_SEARCH_CS_DEV drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_CS_DEV}
+    export REPLICAS="1"        
+elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
+    echo "deploy ${VERSION} to DEV namespace, using HOCS_SEARCH_WCS_DEV drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_WCS_DEV}
+    export REPLICAS="1"
 else
-    if [[ ${ENVIRONMENT} == "qa" ]] ; then
-        echo "deploy ${VERSION} to QA namespace, using HOCS_SEARCH_QA drone secret"
-        export KUBE_TOKEN=${HOCS_SEARCH_QA}
-        export REPLICAS="2"
-    elif [[ ${ENVIRONMENT} == "demo" ]] ; then
-        echo "deploy ${VERSION} to DEMO namespace, using HOCS_SEARCH_DEMO drone secret"
-        export KUBE_TOKEN=${HOCS_SEARCH_DEMO}
-        export REPLICAS="1"
-    elif [[ ${ENVIRONMENT} == "dev" ]] ; then
-        echo "deploy ${VERSION} to DEV namespace, using HOCS_SEARCH_DEV drone secret"
-        export KUBE_TOKEN=${HOCS_SEARCH_DEV}
-        export REPLICAS="1"        
-    else
-        echo "Unable to find environment: ${ENVIRONMENT}"
-    fi
+    echo "Unable to find environment: ${ENVIRONMENT}"
 fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
