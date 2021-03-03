87f2a775afa9fbfdcd6fa4f5b3ab2e3030555723
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2021-03-03 08:43:15 +0000

    HOCS 2492 Create QAX namespace Drone token added to QAX deployment.

--- deploy.sh
+++ deploy.sh
@@ -40,6 +40,9 @@ elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
 elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
     echo "deploy ${VERSION} to DEV namespace, using HOCS_SEARCH_WCS_DEV drone secret"
     export KUBE_TOKEN=${HOCS_SEARCH_WCS_DEV}
+elif [[ ${KUBE_NAMESPACE} == "hocs-qax" ]] ; then
+    echo "deploy ${VERSION} to qax namespace, using HOCS_SEARCH_QAX drone secret"
+    export KUBE_TOKEN=${HOCS_SEARCH_QAX}
 else
     echo "Unable to find environment: ${ENVIRONMENT}"
 fi
