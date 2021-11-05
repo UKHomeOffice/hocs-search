eac37f3774917397027c7dbaf2824fb6d48ca2dd
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-05 14:14:03 +0000

    HOCS-4073 multi-stage docker build with a layered jar instead of a fat jar (#88)
    * This commit moves us to a multi-stage docker build with a layered jar instead of a fat jar. The objective is to reduce the springboot startup time in k8s environments, a side effect is more efficient docker layer caching for developer flow.
    
    Co-authored-by: Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-NAME=${NAME:-search}
-
-JAR=$(find . -name ${NAME}*.jar|head -1)
-exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+exec java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom org.springframework.boot.loader.JarLauncher
