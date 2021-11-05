91b2257ec63808d702e63c4f1c883075e16c92ac
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-05 00:50:20 +0000

    HOCS-3952: remove Junit4 library use

--- src/test/java/uk/gov/digital/ho/hocs/search/api/HocsQueryBuilderTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/search/api/HocsQueryBuilderTest.java
@@ -3,11 +3,11 @@ package uk.gov.digital.ho.hocs.search.api;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.digital.ho.hocs.search.api.dto.DateRangeDto;
 
 import java.util.*;
@@ -17,12 +17,12 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.mockito.ArgumentMatchers.any;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class HocsQueryBuilderTest {
 
     private BoolQueryBuilder bqb;
 
-    @Before
+    @BeforeEach
     public void setup() {
         this.bqb = Mockito.spy(QueryBuilders.boolQuery());
     }
---
+++ src/test/java/uk/gov/digital/ho/hocs/search/aws/listeners/integration/CreateSearchTest.java
@@ -0,0 +1,91 @@
+package uk.gov.digital.ho.hocs.search.aws.listeners.integration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import uk.gov.digital.ho.hocs.search.api.CaseDataService;
+import uk.gov.digital.ho.hocs.search.api.dto.CreateCaseRequest;
+import uk.gov.digital.ho.hocs.search.application.queue.IndexDataChangeRequest;
+import uk.gov.digital.ho.hocs.search.application.queue.DataChangeType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Profile("localstack")
+public class CreateSearchTest extends BaseAwsSqsIntegrationTest {
+
+    @MockBean
+    public CaseDataService caseDataService;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @Test
+    public void consumeMessageFromQueue() throws JsonProcessingException {
+        UUID caseUUID = UUID.randomUUID();
+
+        CreateCaseRequest createCaseRequest = new CreateCaseRequest(
+                UUID.randomUUID(),
+                LocalDateTime.now(),
+                "MIN",
+                "MIN12345",
+                LocalDate.now(),
+                LocalDate.now(),
+                new HashMap());
+
+        String data = objectMapper.writeValueAsString(createCaseRequest);
+        IndexDataChangeRequest request = new IndexDataChangeRequest(caseUUID, data, DataChangeType.CASE_CREATED.value);
+        String message = objectMapper.writeValueAsString(request);
+
+        amazonSQSAsync.sendMessage(searchQueue, message);
+
+        await().until(() -> getNumberOfMessagesOnQueue() == 0);
+        await().untilAsserted(() -> verify(caseDataService).createCase(eq(caseUUID), any()));
+        await().untilAsserted(() -> verifyNoMoreInteractions(caseDataService));
+
+    }
+
+    @Test
+    public void consumeMessageFromQueue_exceptionMakesMessageNotVisible() throws JsonProcessingException {
+        UUID caseUUID = UUID.randomUUID();
+
+        CreateCaseRequest createCaseRequest = new CreateCaseRequest(
+                UUID.randomUUID(),
+                LocalDateTime.now(),
+                "MIN",
+                "MIN12345",
+                LocalDate.now(),
+                LocalDate.now(),
+                new HashMap());
+
+        String data = objectMapper.writeValueAsString(createCaseRequest);
+        IndexDataChangeRequest request = new IndexDataChangeRequest(caseUUID, data, DataChangeType.CASE_CREATED.value);
+        String message = objectMapper.writeValueAsString(request);
+
+        doThrow(new NullPointerException("TEST")).when(caseDataService).createCase(eq(caseUUID), any());
+
+        amazonSQSAsync.sendMessage(searchQueue, message);
+
+        await().until(() -> getNumberOfMessagesOnQueue() == 0);
+        await().until(() -> getNumberOfMessagesNotVisibleOnQueue() == 1);
+
+        await().untilAsserted(() -> verify(caseDataService).createCase(eq(caseUUID), any()));
+        await().untilAsserted(() -> verifyNoMoreInteractions(caseDataService));
+
+    }
+
+}
