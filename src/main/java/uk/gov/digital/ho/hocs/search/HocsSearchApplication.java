b51beeb5de19ae3da7cbf8aa6d5a31cd48f8a013
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-11-05 00:14:29 +0000

    HOCS-3952: remove Retry library use

--- src/main/java/uk/gov/digital/ho/hocs/search/HocsSearchApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/HocsSearchApplication.java
@@ -3,13 +3,11 @@ package uk.gov.digital.ho.hocs.search;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
 import javax.annotation.PreDestroy;
 
 @Slf4j
 @SpringBootApplication
-@EnableRetry
 public class HocsSearchApplication {
 
     public static void main(String[] args) {
