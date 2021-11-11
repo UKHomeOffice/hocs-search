f052886b9fcd6edfac8f15be6d6dd5809be1d412
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-11-11 16:09:21 +0000

    HOCS-4140: rework localstack scripts (#97)
    * HOCS-4140: rework localstack scripts
    
    Currently, to be able to add an index you have to either run curl
    directly or run the `docker-compose up` from within the repository. This
    change splits out the initial setup-aws.sh file into 3 distinct scripts. 
    To set up the elasticsearch index you now only need to run the 3rd
    script. This change also fixes an issue whereby the file does not
    instantiate correctly through drone/docker.
    
    * HOCS-4140: update readme
    
    This change also updates the readme to explain how to use the service
    with localstack.

--- config/localstack/1-setup-queue.sh
+++ config/localstack/1-setup-queue.sh
@@ -13,12 +13,3 @@ done
 
 aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name search-queue-dlq
 aws --endpoint-url=http://localstack:4566 sqs create-queue --queue-name search-queue --attributes '{"RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:search-queue-dlq\",\"maxReceiveCount\":2}"}'
-
-aws --endpoint-url=http://localstack:4578 es create-elasticsearch-domain --domain-name decs --elasticsearch-version 6.7
-
-until curl http://localstack:4571 --silent | grep -q "elasticsearch"; do
-   sleep 10
-   echo "Waiting for ES 6.7.0 to be ready..."
-done
-
-curl -X PUT http://localstack:4571/local-case --silent -H "Content-Type: application/json" -d @/docker-entrypoint-initaws.d/elastic_mapping.json
