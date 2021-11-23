d4b74dded1961fbe1737928c77e5c51de05e83a8
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2021-11-23 11:30:54 +0000

    HOCS-4219: add mapping file check to setup index (#101)
    When the 3-setup-index.sh script is run where the mapping file does not
    currently reside in the running folder an empty mapped index is created.
    This does not work with our system as we require this file to exist.
    This change checks for the existance of the mapping file, and fails out
    if it does not exist.

--- config/localstack/3-setup-index.sh
+++ config/localstack/3-setup-index.sh
@@ -4,7 +4,15 @@ set -euo
 export AWS_ACCESS_KEY_ID=UNSET
 export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
-export ELASTIC_MAPPING_PATH=${ELASTIC_MAPPING_PATH:-"@elastic_mapping.json"}
+
+SCRIPT_LOCATION=$(dirname "$0")
+ELASTIC_MAPPING_PATH="${SCRIPT_LOCATION}/elastic_mapping.json"
+
+if ! [[ -f "${ELASTIC_MAPPING_PATH}" ]]
+then
+    echo "${ELASTIC_MAPPING_PATH} was not found."
+    exit 1
+fi
 
 ## make sure that localstack is running in the pipeline
 until curl http://localstack:4566/health --silent | grep -q "running"; do
@@ -17,4 +25,4 @@ until curl http://localstack:4571 --silent | grep -q "elasticsearch"; do
    echo "Waiting for ElasticSearch to be ready..."
 done
 
-curl -X PUT http://localstack:4571/local-case --silent -H "Content-Type: application/json" -d "${ELASTIC_MAPPING_PATH}"
+curl -X PUT http://localstack:4571/local-case --silent -H "Content-Type: application/json" -d "@${ELASTIC_MAPPING_PATH}"
