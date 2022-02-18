9328e107473cd39a3b13db95d2eecd8fb90515f4
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2022-02-18 07:39:53 +0000

    HOCS-4545: lower the replicas to 1 (#133)
    We know there is an issue within when running multiple replicas whereby
    we risk data loss within the ES index. This change forces the service to
    run with a single replica.

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -6,19 +6,16 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
+export MIN_REPLICAS="1"
+export MAX_REPLICAS="1"
+
 if [[ ${KUBE_NAMESPACE} == *prod ]]
 then
-    export MIN_REPLICAS="2"
-    export MAX_REPLICAS="3"
-    export CLUSTER_NAME="acp-prod"
-
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
+    export CLUSTER_NAME="acp-prod"
 else
-    export MIN_REPLICAS="1"
-    export MAX_REPLICAS="2"
-    export CLUSTER_NAME="acp-notprod"
-
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
+    export CLUSTER_NAME="acp-notprod"
 fi
 
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
