d47633aafb6de6e70d0f641855c4bf483e3ddcb8
Liam MacPherson <liammacpherson1996@gmail.com>
2022-05-18 12:03:22 +0100

    HOCS-5006: allow queue listening disablement
    There is no current way of spinning up the service and not listening to
    the SQS queue. This is due to the same profile being used for the
    Elasticsearch RestHighLevelClient. This change adds a
    `ConditionalOnProperty` that checks for the existance of the `aws.sqs
    .enabled` property.

--- src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/sqs/AwsConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/sqs/AwsConfiguration.java
@@ -1,14 +1,12 @@
-package uk.gov.digital.ho.hocs.search.application.aws.config;
+package uk.gov.digital.ho.hocs.search.application.aws.config.sqs;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +17,8 @@ import org.springframework.context.annotation.Profile;
 @EnableSqs
 @Configuration
 @Profile("localstack")
-public class LocalStackConfiguration {
+@ConditionalOnProperty(prefix = "aws.sqs", value = "enabled", havingValue = "true")
+public class AwsConfiguration {
 
     @Primary
     @Bean
@@ -46,12 +45,5 @@ public class LocalStackConfiguration {
         factory.setWaitTimeOut(5);
 
         return factory;
-    }
-
-    @Bean(destroyMethod = "close")
-    public RestHighLevelClient client(
-            @Value("${aws.es.host}") String host,
-            @Value("${aws.es.port}") int port) {
-        return new RestHighLevelClient(RestClient.builder(new HttpHost(host, port)));
     }
 }
