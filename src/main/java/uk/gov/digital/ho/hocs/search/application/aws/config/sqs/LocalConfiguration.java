2a7b99d93423200cb5df32e5fbd1e8c98d2c7146
Liam MacPherson <liammacpherson1996@gmail.com>
2022-05-18 12:03:22 +0100

    HOCS-5006: allow queue listening disablement
    There is no current way of spinning up the service and not listening to
    the SQS queue. This is due to the same profile being used for the
    Elasticsearch RestHighLevelClient. This change adds a
    `ConditionalOnProperty` that checks for the existance of the `aws.sqs
    .enabled` property.

--- src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/sqs/LocalConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/sqs/LocalConfiguration.java
@@ -1,10 +1,11 @@
-package uk.gov.digital.ho.hocs.search.application.aws.config;
+package uk.gov.digital.ho.hocs.search.application.aws.config.sqs;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.context.annotation.Bean;
@@ -15,14 +16,14 @@ import org.springframework.context.annotation.Profile;
 @EnableSqs
 @Configuration
 @Profile("aws")
-public class SqsConfiguration {
+@ConditionalOnProperty(prefix = "aws.sqs", value = "enabled", havingValue = "true")
+public class LocalConfiguration {
 
     @Primary
     @Bean
     public AmazonSQSAsync awsSqsClient(@Value("${aws.sqs.search.access-key}") String accessKey,
                                        @Value("${aws.sqs.search.secret-key}") String secretKey,
                                        @Value("${aws.region}") String region) {
-
         return AmazonSQSAsyncClientBuilder
                 .standard()
                 .withRegion(region)
