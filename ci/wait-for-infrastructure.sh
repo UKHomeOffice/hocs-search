8cf0de39ce4c1dbbd9edf02817c19fcb0ab3264a
Liam MacPherson <liammacpherson1996@gmail.com>
2022-07-08 10:58:47 +0100

    HOCS-5271: change to healthcheck
    Change to use the health check endpoint and search for the initScripts
    being `initialized`. Initially, this property isn't visible and while
    all initScripts are running the value is `initializing`.

--- ci/wait-for-infrastructure.sh
+++ ci/wait-for-infrastructure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo
 
-until curl 'localhost.localstack.cloud:4566/es/eu-west-2/decs/_cat/indices?v' --silent | grep -q "local-case"; do
-   sleep 5
-   echo "Waiting for Elasticsearch Index to be present..."
+until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
+     sleep 5
+     echo "Waiting for LocalStack Initialisation scripts to have been run..."
 done
