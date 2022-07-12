5dd1f9f2e59ae39984ebbc253584716dc0e5abb8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-12 23:51:07 +0100

    HOCS-5271: Remove existing ci folder

--- ci/wait-for-infrastructure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo
-
-until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
-     sleep 5
-     echo "Waiting for LocalStack Initialisation scripts to have been run..."
-done
