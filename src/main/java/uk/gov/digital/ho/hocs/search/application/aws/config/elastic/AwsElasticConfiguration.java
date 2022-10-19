a1f5cfad025fc11b34c4e1ff62246d8a1141ebf7
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-10-19 09:03:01 +0100

    Change signing library to AWS Auth
    The original signing intercepter library that was used was from January
    2019. This change brings this in line with current AWS documentation on
    how to sign requests.

--- src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/elastic/AwsElasticConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/search/application/aws/config/elastic/AwsElasticConfiguration.java
@@ -1,10 +1,5 @@
 package uk.gov.digital.ho.hocs.search.application.aws.config.elastic;
 
-import com.amazonaws.auth.AWS4Signer;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequestInterceptor;
 import org.elasticsearch.client.RestClient;
@@ -14,6 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
 
 @Configuration
 @Profile("aws")
@@ -25,18 +24,13 @@ public class AwsElasticConfiguration {
                                       @Value("${aws.region}") String region,
                                       @Value("${aws.es.access-key}") String accessKey,
                                       @Value("${aws.es.secret-key}") String secretKey) {
+        Aws4Signer signer = Aws4Signer.create();
+        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(accessKey, secretKey));
 
-        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(
-            new BasicAWSCredentials(accessKey, secretKey));
-
-        AWS4Signer signer = new AWS4Signer();
-        signer.setServiceName(serviceName);
-        signer.setRegionName(region);
-
-        HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer,
-            credentialsProvider);
-        RestClientBuilder builder = RestClient.builder(new HttpHost(host, -1, "https"));
-        builder.setHttpClientConfigCallback(
+        HttpRequestInterceptor interceptor = new AwsRequestSigningApacheInterceptor(serviceName, signer,
+            credentialsProvider, region);
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, -1, "https")).setHttpClientConfigCallback(
             httpClientBuilder -> httpClientBuilder.useSystemProperties().addInterceptorLast(interceptor));
 
         return new RestHighLevelClient(builder);
